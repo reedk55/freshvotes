30533e82cf4c0c2c463dbf1c597437fca9220822
Katherine Reed <reedkatherine2@gmail.com>
2020-12-02 18:32:48 -0500

    Initial check-in of the FreshVotes code

---
+++ src/main/java/com/reedk55/freshvotes/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.reedk55.freshvotes;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FreshvotesApplication.class);
+    }
+
+}
---
+++ src/test/java/com/reedk55/freshvotes/FreshvotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reedk55.freshvotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshvotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
