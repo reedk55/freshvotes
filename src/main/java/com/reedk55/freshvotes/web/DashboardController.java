3454de8ffc986bc5ded455c6051aab64c815f2e2
Katherine Reed <reedkatherine2@gmail.com>
2020-12-02 22:45:19 -0500

    Creating custom login page

---
+++ src/main/java/com/reedk55/freshvotes/web/DashboardController.java
@@ -0,0 +1,13 @@
+package com.reedk55.freshvotes.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/")
+    public String rootView () {
+        return "index";
+    }
+}
